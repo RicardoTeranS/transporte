e3c63055b9fd761c999a76b3acfd2c40cde34623
Ricardo Teran <108688521+RicardoTeranS@users.noreply.github.com>
2023-01-31 15:11:38 -0400

    arreglo de nombre

--- transportes/__manifest__.py
+++ transportes/__manifest__.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Transportes",
+    'name': "Transporte y Flete",
 
     'summary': """
-        Módulo para la gestión de transportes""",
+        Módulo para la gestión de transportes y Fletes""",
 
     'description': """
-        Gestión de camiones, conductores y los viajes que estos realizan.
+        Gestión de camiones, conductores y los viajes que estos realizan,
+        Gestión de Carga y descarga de paquetes.
     """,
 
-    'author': "A20AdrianLP",
-    'website': "www.iessanclemente.net",
+    'author': "RicardoTeranNet",
+    'website': "www.ricardoterannet.net",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
