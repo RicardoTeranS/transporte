9144de4cc11542a8dfb423e7c17c5e3080a21603
Ricardo Teran <108688521+RicardoTeranS@users.noreply.github.com>
2023-01-31 15:35:34 -0400

    URL

--- transportes/__manifest__.py
+++ transportes/__manifest__.py
@@ -11,7 +11,7 @@
     """,
 
     'author': "RicardoTeranNet",
-    'website': "www.ricardoterannet.net",
+    'website': "www.ricardoteran.net",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
